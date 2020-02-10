2806cd83d2ad270b5473938d1eb81a5522499767
Teotonio S Carvalho <teo.decarvalho@gmail.com>
2020-02-10 13:21:17 -0300

    Added step variable

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+# Importing libraries
 import cv2
 import serial
 import time
 from PIL import Image
 import os
 
+# Setting up the microscope stage
 stage = serial.Serial('COM4', 115200, timeout=3)
 time.sleep(2)
 stage.write(b"\r\n\r\n")
 
-
+# Defining functions
 def send_gcode_str(gcode_str):
     stage.flushInput()
     time.sleep(.01)
@@ -52,23 +54,30 @@ def move_down(step_size):
     send_gcode_str("G21 G91 G0 Z-" + str(step_size))
     send_gcode_str("G90")
 
-
+# The app camera cannot be opened with this camera
 cap = cv2.VideoCapture(0)
 reset_zero()
-for row in range(5):
+step = .5
+n_steps = 5
+for row in range(n_steps):
     time.sleep(3)
-    move_fwd(.5)
-    for col in range(5):
-        move_left(.5)
+    move_fwd(step)
+    for col in range(n_steps):
+        move_left(step)
         time.sleep(1)
+        # Taking a composite snapshot
         frame = .1 * cap.read()[1]
         for i in range(9):
             frame += .1 * cap.read()[1] # return a single frame in variable `frame`
+        # Writing the composite image as PNG to current folder
         cv2.imwrite('c%d_%d.png'%(row, col), frame)
+        # Converting the files to tiff and saving in ./images folder
         Image.open('c%d_%d.png'%(row, col)).save('./images/c%d_%d.tif'%(row, col))
         os.remove('c%d_%d.png'%(row, col))
-    move_right(5 * .5)
+    # Returning to the start of the row
+    move_right(n_steps * step)
     time.sleep(2)
+
 cap.release()
 
 
