68aaa464e21e03b95dc090ff8b0c6d9cd29415c0
Teo <teo.decarvalho@gmail.com>
2021-02-08 18:17:06 -0300

    Added a stage class

---
+++ classes.py
@@ -0,0 +1,110 @@
+import serial
+from time import sleep
+from math import ceil
+import cv2
+
+class Stage():
+    def __init__(self, port):
+        self.connect(port)
+        self.go_home()
+        self.wait_move()
+
+    def connect(self, port):
+        self.serial = serial.Serial(port, 115200, timeout=3)
+        sleep(3)
+
+    def send_gcode_str(self, gcode_str, debug = False):
+        debug = True
+        self.serial.flushInput()
+        sleep(.01)
+        if debug:
+            print(gcode_str)
+        gcode_str += "\n"
+        self.serial.write(gcode_str.encode())
+        grbl_out = self.serial.readline()  # Wait for robot response with carriage return
+        if debug:
+            print(grbl_out.strip())
+        return (grbl_out.strip().decode("ascii"))
+
+    def is_done(self):
+        status = self.send_gcode_str("G4 P0")
+        if status == '':
+            self.send_gcode_str("?")
+        return status == 'ok'
+
+    def go_home(self):
+        self.send_gcode_str("$H")
+
+    def wait_move(self):
+        while not self.is_done():
+            sleep(.5)
+            pass
+        return True
+
+    def move_x(self, x):
+        self.send_gcode_str(f"G90 X{x}")
+        return self.wait_move()
+
+    def move_y(self, y):
+        self.send_gcode_str(f"G90 Y{-y}")
+        return self.wait_move()
+
+    def scan(self, xend, yend, step_x, step_y, task, xstart = 0, ystart = 0):
+        range_x = xend - xstart
+        range_y = yend - ystart
+        nsteps_x = ceil(range_x/float(step_x)) + 1
+        nsteps_y = ceil(range_y/ float(step_y)) + 1
+        flip = False
+        for y in range(nsteps_y):
+            self.move_y(ystart + y * step_y)
+            if flip:
+                x_seq = range(nsteps_x)[::-1]
+            else:
+                x_seq = range(nsteps_x)
+            flip = not flip
+            for x in x_seq:
+                self.move_x(xstart + x * step_x)
+                task(x = x*step_x, y = y*step_y)
+
+
+class Camera():
+    def __init__(self, camera):
+        self.connect(camera=camera)
+
+    def connect(self, camera):
+        self.cap = cv2.VideoCapture(camera)
+
+    def capture(self):
+        frame = self.cap.read()[1]#.1 * self.cap.read()[1]
+        # for i in range(9):
+        #     frame += .1 * cap.read()[1] # return a single frame in variable `frame`
+        return frame
+
+    def save(self, image_obj, filepath):
+        cv2.imwrite(filepath, image_obj)
+
+# # The app camera cannot be opened with this camera
+# cap = cv2.VideoCapture(0)
+# reset_zero()
+# step = .5
+# n_steps = 5
+# for row in range(n_steps):
+#     time.sleep(3)
+#     move_fwd(step)
+#     for col in range(n_steps):
+#         move_left(step)
+#         time.sleep(1)
+#         # Taking a composite snapshot
+#         frame = .1 * cap.read()[1]
+#         for i in range(9):
+#             frame += .1 * cap.read()[1] # return a single frame in variable `frame`
+#         # Writing the composite image as PNG to current folder
+#         cv2.imwrite('c%d_%d.png'%(row, col), frame)
+#         # Converting the files to tiff and saving in ./images folder
+#         Image.open('c%d_%d.png'%(row, col)).save('./images/c%d_%d.tif'%(row, col))
+#         os.remove('c%d_%d.png'%(row, col))
+#     # Returning to the start of the row
+#     move_right(n_steps * step)
+#     time.sleep(2)
+#
+# cap.release()
--- main.py
+++ main.py
@@ -1,91 +1,54 @@
 # Importing libraries
-import cv2
-import serial
-import time
-from PIL import Image
-import os
+#import cv2
+#import serial
+from time import sleep
+#from PIL import Image
+#import os
 
-# Setting up the microscope stage
-stage = serial.Serial('COM4', 115200, timeout=3)
-time.sleep(2)
-stage.write(b"\r\n\r\n")
-
-# Defining functions
-def send_gcode_str(gcode_str):
-    stage.flushInput()
-    time.sleep(.01)
-    print(gcode_str)
-    gcode_str += "\n"
-    stage.write(gcode_str.encode())
-    grbl_out = stage.readline()  # Wait for robot response with carriage return
-    print(grbl_out.strip())
-    return (grbl_out.strip().decode("ascii"))
+from classes import Stage, Camera
+#stage = Stage("/dev/tty.usbserial-1460")
+camera = Camera(0)
+if __name__ == "__main__":
+    image = camera.capture()
+    camera.save(image,  "teste.png")
+    # def task(x, y):
+    #     print(x, y)
+    #     sleep(1)
+    # stage.scan(xend=10, yend=10, xstart=0, ystart=0, step_x=1, step_y=1, task = task)
 
 
-def reset_zero():
-    send_gcode_str("G10 P0 L20 X0 Y0 Z0")
-
-
-def return_to_zero():
-    send_gcode_str("G90 G0 Z1.0")
-    send_gcode_str("G90 G0 X0 Y0 Z0")
-
-def move_left(step_size):
-    send_gcode_str("G21 G91 G0 X-" + str(step_size))
-    send_gcode_str("G90")
-
-def move_right(step_size):
-    send_gcode_str("G21 G91 G0 X" + str(step_size))
-    send_gcode_str("G90")
-
-def move_fwd(step_size):
-    send_gcode_str("G21 G91 G0 Y" + str(step_size))
-    send_gcode_str("G90")
-
-def move_rev(step_size):
-    send_gcode_str("G21 G91 G0 Y-" + str(step_size))
-    send_gcode_str("G90")
-
-def move_up(step_size):
-    send_gcode_str("G21 G91 G0 Z" + str(step_size))
-    send_gcode_str("G90")
-
-def move_down(step_size):
-    send_gcode_str("G21 G91 G0 Z-" + str(step_size))
-    send_gcode_str("G90")
-
-# The app camera cannot be opened with this camera
-cap = cv2.VideoCapture(0)
-reset_zero()
-step = .5
-n_steps = 5
-for row in range(n_steps):
-    time.sleep(3)
-    move_fwd(step)
-    for col in range(n_steps):
-        move_left(step)
-        time.sleep(1)
-        # Taking a composite snapshot
-        frame = .1 * cap.read()[1]
-        for i in range(9):
-            frame += .1 * cap.read()[1] # return a single frame in variable `frame`
-        # Writing the composite image as PNG to current folder
-        cv2.imwrite('c%d_%d.png'%(row, col), frame)
-        # Converting the files to tiff and saving in ./images folder
-        Image.open('c%d_%d.png'%(row, col)).save('./images/c%d_%d.tif'%(row, col))
-        os.remove('c%d_%d.png'%(row, col))
-    # Returning to the start of the row
-    move_right(n_steps * step)
-    time.sleep(2)
-
-cap.release()
-
-
-for j in range(5):
-    images = []
-    for i in range(5):
-        image = cv2.imread("./images/c%d_%d.tif"%(i, j))
-        images.append(image)
-    stitcher = cv2.Stitcher_create()
-    (status, stitched) = stitcher.stitch(images)
-    cv2.imwrite("test%d.png"%j, stitched)
+# # The app camera cannot be opened with this camera
+# cap = cv2.VideoCapture(0)
+# reset_zero()
+# step = .5
+# n_steps = 5
+# for row in range(n_steps):
+#     time.sleep(3)
+#     move_fwd(step)
+#     for col in range(n_steps):
+#         move_left(step)
+#         time.sleep(1)
+#         # Taking a composite snapshot
+#         frame = .1 * cap.read()[1]
+#         for i in range(9):
+#             frame += .1 * cap.read()[1] # return a single frame in variable `frame`
+#         # Writing the composite image as PNG to current folder
+#         cv2.imwrite('c%d_%d.png'%(row, col), frame)
+#         # Converting the files to tiff and saving in ./images folder
+#         Image.open('c%d_%d.png'%(row, col)).save('./images/c%d_%d.tif'%(row, col))
+#         os.remove('c%d_%d.png'%(row, col))
+#     # Returning to the start of the row
+#     move_right(n_steps * step)
+#     time.sleep(2)
+#
+# cap.release()
+#
+#
+# for j in range(5):
+#     images = []
+#     for i in range(5):
+#         image = cv2.imread("./images/c%d_%d.tif"%(i, j))
+#         images.append(image)
+#     stitcher = cv2.Stitcher_create()
+#     (status, stitched) = stitcher.stitch(images)
+#     cv2.imwrite("test%d.png"%j, stitched)
